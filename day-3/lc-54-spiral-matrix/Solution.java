a297ce71b8dbab637724f06cfabd4ff13d2d81bd
zahyaah <itszayd2003@gmail.com>
2024-10-19 15:53:15 +0530

    update lc-54

--- day-3/lc-54-spiral-matrix/Solution.java
+++ day-3/lc-54-spiral-matrix/Solution.java
@@ -11,15 +11,19 @@ public class Solution {
         int left = 0, right = m-1; 
 
         while (top <= bottom && left <= right) {
+            // top -> left to right
             for (int i = left; i <= right; i++) {
                 list.add(matrix[top][i]);
             }
             top++; 
+
+            // right -> top to bottom
             for (int i = top; i <= bottom; i++) {
                 list.add(matrix[i][right]);
             }
             right--; 
             
+            // bottom -> right to left
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     list.add(matrix[bottom][i]);
@@ -27,6 +31,7 @@ public class Solution {
                 bottom--; 
             }
             
+            // left -> bottom to top
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     list.add(matrix[i][left]);
