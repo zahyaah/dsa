a62128f31e682d07c1e0a4f20cc5b82e349bd103
zahyaah <itszayd2003@gmail.com>
2024-10-24 00:53:34 +0530

    gfg-product-array-puzzle

---
+++ day-8/gfg-product-array-puzzle/Solution.java
@@ -0,0 +1,39 @@
+// link - https://www.geeksforgeeks.org/problems/product-array-puzzle4525/1
+
+import java.util.Arrays;
+
+public class Solution {
+    public long[] productExceptSelf(int nums[]) {
+        int n = nums.length; 
+        long[] result = new long[n]; 
+        long prefixProduct = 1, postfixProduct = 1; 
+        
+        // prefix product for an element i
+        // is the product of all the elements before i 
+        for (int i = 0; i < n; i++) {
+            result[i] = prefixProduct; 
+            prefixProduct *= nums[i]; 
+        }
+        
+        // postfix product for an element j 
+        // is the product of all the elements after i
+        for (int j = n-1; j >= 0; j--) {
+            result[j] *= postfixProduct; 
+            postfixProduct *= nums[j]; 
+        }
+        
+        return result; 
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution(); 
+
+        int[] ts1 = new int[] {10, 3, 5, 6, 2}; 
+        int[] ts2 = new int[] {12, 0}; 
+
+        long[] res1 = sol.productExceptSelf(ts1);
+        long[] res2 = sol.productExceptSelf(ts2);
+
+        System.out.println(Arrays.toString(res1));
+        System.out.println(Arrays.toString(res2));
+    }
+}
