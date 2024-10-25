71675caa2ed3050effc8ad64c500038a4f7b2686
zahyaah <itszayd2003@gmail.com>
2024-10-25 17:38:47 +0530

    visited not required

--- day-9/gfg-find-the-number-of-islands/Solution.java
+++ day-9/gfg-find-the-number-of-islands/Solution.java
@@ -3,20 +3,19 @@ public class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length; 
         int m = grid[0].length; 
-        boolean[][] visited = new boolean[n][m]; 
         int count = 0; 
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
                     count++; 
-                    traverse(grid, visited, i, j, n, m);
+                    traverse(grid, i, j, n, m);
                 }
             }
         }
         return count; 
     }   
-    public void traverse(char[][] grid, boolean[][] visited, int row, int col, int n, int m) {
+    public void traverse(char[][] grid, int row, int col, int n, int m) {
         int[][] directions = {
             {0, 1}, // right
             {1, 0}, // bottom
@@ -27,15 +26,14 @@ public class Solution {
             {1, -1}, // bottom diagonally left
             {-1, 1} // top diagonally right 
         };
-        visited[row][col] = true; 
         grid[row][col] = '-';
         
         for (int i = 0; i < 8; i++) {
             int newRow = row + directions[i][0]; 
             int newCol = col + directions[i][1]; 
             
-            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && visited[newRow][newCol] == false && grid[newRow][newCol] == '1') {
-                traverse(grid, visited, newRow, newCol, n, m);
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1') {
+                traverse(grid, newRow, newCol, n, m);
             }
         }
     }
