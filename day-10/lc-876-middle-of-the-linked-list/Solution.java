d5d7e98558e33aeb25d1e70f8cf4e3d70fdba7d1
zahyaah <itszayd2003@gmail.com>
2024-10-26 19:37:40 +0530

    lc-876

---
+++ day-10/lc-876-middle-of-the-linked-list/Solution.java
@@ -0,0 +1,43 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution(); 
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = sol.middleNode(head);
+
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next; 
+        }
+    }
+}
